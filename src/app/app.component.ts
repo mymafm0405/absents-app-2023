b1deeda37661e39ca9bb1baf6691d9d7037ace63
Mahmoud <mymafm0405@gmail.com>
2023-01-08 11:45:23 +0300

    start the design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  showManage = false;
+  showInsert = false;
+
+  onManage() {
+    this.showManage = true;
+    this.showInsert = false;
+  }
+
+  onInsert() {
+    this.showManage = false;
+    this.showInsert = true;
+  }
 }
