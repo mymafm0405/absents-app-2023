3fd387bfc260ec28c4a24229270555988a886ca2
Mahmoud <mymafm0405@gmail.com>
2023-01-08 10:56:50 +0300

    add some grades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { StudentAbsentLateComponent } from './components/student-absent-late/student-absent-late.component';
 import { StudentReportComponent } from './components/student-report/student-report.component';
 import { StudentManageComponent } from './components/student-manage/student-manage.component';
+import { GradeComponent } from './components/grade/grade.component';
+import { ClassComponent } from './components/class/class.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { StudentManageComponent } from './components/student-manage/student-mana
     StudentAbsentLateComponent,
     StudentReportComponent,
     StudentManageComponent,
+    GradeComponent,
+    ClassComponent,
   ],
   imports: [
     BrowserModule
--- src/app/shared/students.service.ts
+++ src/app/shared/students.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Class } from './class.model';
+import { Grade } from './grade.model';
 import { Student } from './student.model';
 
 @Injectable({
@@ -6,11 +8,30 @@ import { Student } from './student.model';
 })
 export class StudentsService {
   students: Student[] = [
-    new Student('1', 'Mahmoud Yhya', 7, 1, 66548110, false, false, ''),
-    new Student('1', 'Basem Yhya', 7, 1, 50001953, false, false, ''),
+    new Student('1', 'Mahmoud Yhya', 7, 1, 66548110, false, false, '', '2023-01-08'),
+    new Student('2', 'Basem Yhya', 8, 1, 50001953, false, false, '', '2023-01-08'),
   ];
+
+  grades: Grade[] = [
+    new Grade('g7', 7, 'Grade 7'),
+    new Grade('g8', 8, 'Grade 8'),
+    new Grade('g9', 9, 'Grade 9'),
+  ]
+
+  classes: Class[] = [
+    new Class('1', 'g7', 1, 'Class 1'),
+    new Class('2', 'g8', 1, 'Class 1'),
+  ]
 
   getStudents() {
     return this.students;
+  }
+
+  getGrades() {
+    return this.grades;
+  }
+
+  getClasses() {
+    return this.classes;
   }
 }
